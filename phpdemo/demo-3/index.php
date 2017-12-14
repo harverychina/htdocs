1717a7479c39ec0d376f64d869ff6a14775b056f
黄健聪 <harverychina@126.com>
2017-12-14 20:09:51 +0800

    20171214 OK

---
+++ phpdemo/demo-3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>简易计算器--加法</title>
+</head>
+<body>
+    <form action="">
+        第一操作数：<input type="text" name="a" id="a"><br/>
+        +<br/>
+        第二操作数：<input type="text" name="b" id="a"><br/>
+        =<br/>
+    </form>
+</body>
+</html>
