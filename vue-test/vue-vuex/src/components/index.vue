8e4ca31c078e3d44c82ca9217054a95b8329d895
黄健聪 <harverychina@126.com>
2018-05-31 15:37:32 +0800

    2018-5-31 add new vue-vuex

---
+++ vue-test/vue-vuex/src/components/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h3>Vue.js Vuex Sample</h3>
+    </div>
+</template>
+<script>
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+var myStore = new Vuex.Store({
+  // store对象
+  state: {
+    show: false
+  }
+});
+
+export default {
+  data() {
+    return {
+      name: "jack"
+    };
+  },
+  store: myStore,
+  computed: {
+    get() {
+      return this.$store.name;
+    }
+  },
+  mounted() {
+    console.log(this.$store.state.show);
+  }
+};
+</script>
+<style>
+</style>
